6e9dcd65ffea3b079492cd95cfe84907607ebdda
Lyna <git@blooym.dev>
2025-10-17 13:08:31 +0200

    refactor: remove duplicate CID checking
    A ratelimiter would work better for this use-case anyway and can be done at the reverse proxy level

--- src/Api/src/extractors/content_id.rs
+++ src/Api/src/extractors/content_id.rs
@@ -3,22 +3,11 @@ use axum::{
     http::{HeaderMap, StatusCode, request::Parts},
     response::{IntoResponse, Response},
 };
-use moka::future::Cache;
-use std::sync::{Arc, LazyLock};
 
 const CONTENT_ID_HASH_HEADER: &str = "x-content-id-hash";
 const CONTENT_ID_SALT_HEADER: &str = "x-content-id-salt";
 const CONTENT_ID_HASH_LENGTH: usize = 64;
 const CONTENT_ID_SALT_LENGTH: usize = 32;
-
-static CONTENT_ID_HASH_CACHE: LazyLock<Arc<Cache<Box<str>, ()>>> = LazyLock::new(|| {
-    const DEFAULT_CACHE_CAPACITY: u64 = 1000;
-    Arc::new(Cache::new(
-        std::env::var("GOODFRIEND_CONTENT_ID_CACHE_CAPACITY")
-            .map(|v| v.parse().unwrap_or(DEFAULT_CACHE_CAPACITY))
-            .unwrap_or(DEFAULT_CACHE_CAPACITY),
-    ))
-});
 
 pub struct UniqueContentId {
     pub hash: Box<str>,
@@ -30,7 +19,6 @@ pub enum ContentIdExtractError {
     HashMissing,
     SaltMissing,
     HashOrSaltInvalid,
-    ContentIdDuplicate,
 }
 
 impl<S> FromRequestParts<S> for UniqueContentId
@@ -58,14 +46,6 @@ where
             return Err(ContentIdExtractError::HashOrSaltInvalid);
         }
 
-        // Ensure duplicate ContentId requests are not processed.
-        if CONTENT_ID_HASH_CACHE.contains_key(&content_id_hash) {
-            return Err(ContentIdExtractError::ContentIdDuplicate);
-        }
-        CONTENT_ID_HASH_CACHE
-            .insert(content_id_hash.clone(), ())
-            .await;
-
         Ok(Self {
             hash: content_id_hash,
             salt: content_id_salt,
@@ -79,7 +59,6 @@ impl IntoResponse for ContentIdExtractError {
             Self::HashMissing | Self::SaltMissing | Self::HashOrSaltInvalid => {
                 (StatusCode::BAD_REQUEST, "Invalid ContentId data")
             }
-            Self::ContentIdDuplicate => (StatusCode::CONFLICT, "Duplicated ContentID request data"),
         }
         .into_response()
     }
